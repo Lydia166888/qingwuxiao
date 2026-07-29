da297d35b3e21aa521d887491e316131b2c713da
Lydia166888 <Lydia166888@users.noreply.github.com>
2026-07-29 16:25:46 +0800

    V25: GitHub Gist云同步 + 财务明细周月年全部视图 + 图表月年维度 + 分类排行按周期

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* Service Worker - 青梧笑 离线缓存 v24.4 */
-const CACHE_NAME = "qingwuxiao-v24-4";
+/* Service Worker - 青梧笑 离线缓存 v25 */
+const CACHE_NAME = "qingwuxiao-v25";
 const ASSETS = [
   "./",
   "./index.html",
